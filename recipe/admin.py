87caa4804c8bfa5c7657a52c7929426f8a4e4c22
Daniel <daniel@testpress.in>
2022-06-27 19:13:20 +0530

    Create recipe model & add model to admin site

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Cuisine, Ingredient, Collection, Recipe
 
 # Register your models here.
+
+
+@admin.register(Cuisine)
+class CuisineAdmin(admin.ModelAdmin):
+    list_display = ['name', ]
+
+
+@admin.register(Ingredient)
+class IngredientsAdmin(admin.ModelAdmin):
+    list_display = ['name', 'quantity', 'measurement', 'optional']
+
+
+@admin.register(Recipe)
+class ReceipeAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'type',
+                    'image', 'cuisine', 'difficulty',
+                    'instructions', 'servings', 'preparation_time',
+                    'total_time', 'calories']
+    search_fields = ('title', 'author', 'type')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title', ]
--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,66 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
+
+
+class Cuisine (models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=250)
+    quantity = models.IntegerField()
+    measurement = models.CharField(blank=True, max_length=25)
+    optional = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.name} : {self.quantity}{self.measurement} | {self.optional}'
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='recipe_author')
+    TYPE_CHOICES = (
+        ('veg', 'VEG'),
+        ('non-veg', 'NON-VEG'),
+        ('vegan', "VEGAN"),
+    )
+    type = models.CharField(max_length=10,
+                            choices=TYPE_CHOICES,
+                            default='veg')
+    image = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True)
+    cuisine = models.ForeignKey(Cuisine, on_delete=models.CASCADE,
+                                related_name='style_of_cooking')
+    difficulty = models.IntegerField(validators=[
+        MinValueValidator(1), MaxValueValidator(10)])
+    ingredients = models.ManyToManyField(
+        Ingredient, related_name='ingredients_list')
+    instructions = models.TextField()
+    servings = models.IntegerField(validators=[
+        MinValueValidator(1), MaxValueValidator(10)])
+    preparation_time = models.CharField(max_length=25)
+    total_time = models.CharField(max_length=25)
+    calories = models.IntegerField(validators=[MaxValueValidator(750)])
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return f'{self.title}| {self.type}| {self.cuisine}'
+
+
+class Collection(models.Model):
+    title = models.CharField(max_length=250)
+    recipes = models.ManyToManyField(
+        Recipe, related_name='recipe_collections')
+
+    def __str__(self):
+        return self.title
