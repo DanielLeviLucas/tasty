c6685bda395be5076c49c4850df8e6ac684a67c4
Daniel <daniel@testpress.in>
2022-06-29 16:03:16 +0530

    Alter field author on recipe

--- recipe/models.py
+++ recipe/models.py
@@ -16,7 +16,7 @@ class Recipe(TimeStampedModel):
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     author = models.ForeignKey(User, on_delete=models.CASCADE,
-                               related_name='recipe_author')
+                               related_name='author_recipes')
     TYPE_CHOICES = (
         ('veg', 'VEG'),
         ('non-veg', 'NON-VEG'),
